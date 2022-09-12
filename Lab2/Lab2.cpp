7791c510499860f5797e50cf17975fbdd13f30fe
JUNSOOCHO <j082175@naver.com>
2022-09-12 11:39:23 +0900

    first project

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -6,7 +6,10 @@ namespace lab2
 {
 	void PrintIntegers(std::istream& in, std::ostream& out)
 	{
-		unsigned int value[128] = { 0, };
+		unsigned int maxSize = 128;
+
+		//int value[128] = { 0, };
+		int* value = new int[maxSize];
 		unsigned int count = 0;
 
 		while (!in.eof())
@@ -18,22 +21,33 @@ namespace lab2
 				break;
 			}
 
-			if (in.fail() || value[count] > std::numeric_limits<int>::max())
+			if (in.fail())
 			{
-				//in.clear();
-				//in.ignore(std::numeric_limits<long long>::max(), ' ');
-				//in.ignore(std::numeric_limits<long long>::max(), '\n');
+				in.clear();
+				in.ignore();
 				continue;
 			}
 
 			count++;
 
+			if (count >= maxSize)
+			{
+				int* temp = new int[maxSize * 2];
+				for (size_t i = 0; i < maxSize; i++)
+				{
+					temp[i] = value[i];
+				}
+
+				delete[] value;
+				maxSize *= 2;
+				value = temp;
+			}
+
 		}
 
 		value[count] = std::numeric_limits<unsigned int>::max();
 
-
-
+		
 		out << std::right;
 		out << std::setw(12) << "oct" <<
 			std::setw(11) << "dec" <<
@@ -52,10 +66,81 @@ namespace lab2
 				std::setw(10) << std::hex << std::uppercase << value[index] << std::endl;
 			index++;
 		}
+
+		delete[] value;
 	}
 
 	void PrintMaxFloat(std::istream& in, std::ostream& out)
 	{
+		unsigned int maxSize = 8;
 
+		//float value[128] = { 0, };
+		float* value = new float[maxSize];
+
+		unsigned int count = 0;
+
+		while (!in.eof())
+		{
+			in >> value[count];
+
+			if (in.eof())
+			{
+				break;
+			}
+
+			if (in.fail())
+			{
+				in.clear();
+				in.ignore();
+				continue;
+			}
+
+			count++;
+
+			if (count >= maxSize)
+			{
+				float* temp = new float[maxSize * 2];
+				for (size_t i = 0; i < maxSize; i++)
+				{
+					temp[i] = value[i];
+				}
+
+				delete[] value;
+				maxSize *= 2;
+				value = temp;
+			}
+		}
+
+		value[count] = std::numeric_limits<float>::max();
+
+		out << std::internal << std::showpos << std::showpoint << std::fixed << std::setprecision(3);
+
+		int index = 0;
+
+		while (value[index] != std::numeric_limits<float>::max())
+		{
+			out << std::setw(5) << " " <<
+				std::setw(15) << value[index] << std::endl;
+			index++;
+		}
+
+		index = 0;
+		float max = 0.f;
+
+		while (value[index] != std::numeric_limits<float>::max())
+		{
+			if (max < value[index])
+			{
+				max = value[index];
+			}
+			
+			index++;
+		}
+
+		out << std::setw(5) << std::left << "max:" << std::internal <<
+			std::setw(15) << max << std::endl;
+
+
+		delete[] value;
 	}
 }
