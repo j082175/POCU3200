5f8da74a925630254f69eee398876ca32145450f
JUNSOOCHO <j082175@naver.com>
2022-11-08 10:50:15 +0900

    12

--- Lab8/FixedBoolVector.h
+++ Lab8/FixedBoolVector.h
@@ -26,44 +26,69 @@ namespace lab8
 
 
 	private:
-		size_t mCurrentPosition;
+		enum
+		{
+			CONTROL_POINT = 8
+		};
+
+		size_t mCount;
+		// 8∫Ò∆Æ
 		int32_t mArr[N];
+
+		const unsigned char option0 = 1 << 0; // 0000 0001 
+		const unsigned char option1 = 1 << 1; // 0000 0010
+		const unsigned char option2 = 1 << 2; // 0000 0100
+		const unsigned char option3 = 1 << 3; // 0000 1000
+		const unsigned char option4 = 1 << 4; // 0001 0000
+		const unsigned char option5 = 1 << 5; // 0010 0000
+		const unsigned char option6 = 1 << 6; // 0100 0000
+		const unsigned char option7 = 1 << 7; // 1000 0000
 	};
 
 	template<size_t N>
 	inline FixedVector<bool, N>::FixedVector()
-		: mCurrentPosition(0)
+		: mCount(0)
 		, mArr{ 0 }
 	{}
 
 	template<size_t N>
 	inline bool FixedVector<bool, N>::Add(bool t)
 	{
-		if (mCurrentPosition >= N)
+		if (mCount >= N)
 		{
 			return false;
 		}
 
-		mArr[mCurrentPosition++] = static_cast<int32_t>(t);
+		if (t)
+		{
+			unsigned char move = 1;
+			move = move << mCount % CONTROL_POINT;
+
+			mArr[mCount / CONTROL_POINT] += move;
+		}
+
+		++mCount;
 		return true;
 	}
 
 	template<size_t N>
 	inline bool FixedVector<bool, N>::Remove(bool t)
 	{
+		unsigned char move = 1;
 
-		for (size_t i = 0; i < mCurrentPosition; i++)
+
+		for (size_t i = 0; i < mCount / CONTROL_POINT + mCount % CONTROL_POINT; i++)
 		{
-			if (mArr[i] == static_cast<int32_t>(t))
+			for (size_t j = 0; j < CONTROL_POINT; j++)
 			{
-				for (size_t j = i; j < mCurrentPosition - 1; j++)
+				if (mArr[mCount / CONTROL_POINT] & move)
 				{
-					mArr[j] = mArr[j + 1];
+
 				}
-				--mCurrentPosition;
-				return true;
+				move = move << 1;
 			}
 		}
+
 
 		return false;
 	}
@@ -80,7 +105,7 @@ namespace lab8
 	inline int FixedVector<bool, N>::GetIndex(bool t)
 	{
 
-		for (size_t i = 0; i < mCurrentPosition; i++)
+		for (size_t i = 0; i < mCount; i++)
 		{
 			if (mArr[i] == static_cast<int32_t>(t))
 			{
@@ -94,7 +119,7 @@ namespace lab8
 	template<size_t N>
 	inline size_t FixedVector<bool, N>::GetSize()
 	{
-		return mCurrentPosition;
+		return mCount;
 	}
 
 	template<size_t N>
--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -6,6 +6,252 @@
 
 using namespace lab8;
 
+enum
+{
+	CAPACITY = 16
+};
+
+void test1()
+{
+	// empty vector test in primitive type
+	{
+		FixedVector<int, CAPACITY> v1;
+
+		assert(!v1.Remove(10));
+		assert(v1.GetIndex(10) == -1);
+		assert(v1.GetSize() == 0);
+		assert(v1.GetCapacity() == CAPACITY);
+	}
+
+	{
+		FixedVector<std::string, CAPACITY> v1;
+		std::string cpu("CPU");
+
+		assert(!v1.Remove(cpu));
+		assert(v1.GetIndex(cpu) == -1);
+		assert(v1.GetSize() == 0);
+		assert(v1.GetCapacity() == CAPACITY);
+	}
+
+	// vector, whose capacity is one, test in primitive type
+
+	{
+
+		FixedVector<int, 1> v1;
+
+		assert(v1.GetCapacity() == 1);
+
+		assert(v1.GetSize() == 0);
+
+		assert(v1.Add(100));
+		assert(v1.GetSize() == 1);
+
+		assert(!v1.Add(200));
+		assert(v1.GetSize() == 1);
+
+		assert(!v1.Remove(200));
+		assert(v1.GetSize() == 1);
+
+		assert(v1.Remove(100));
+		assert(v1.GetSize() == 0);
+
+		assert(v1.Add(100));
+		assert(v1.GetSize() == 1);
+
+		assert(v1.Get(0) == 100);
+		assert(v1[0] == 100);
+
+		v1[0] = 200;
+
+		assert(v1.Get(0) == 200);
+		assert(v1.GetIndex(100) == -1);
+		assert(v1.GetIndex(200) == 0);
+	}
+
+
+	// vector, whose capacity is one, test in class type
+	{
+		FixedVector<std::string, 1> v1;
+
+		std::string cpu("CPU");
+		std::string cpp("C++");
+
+		assert(v1.GetCapacity() == 1);
+
+		assert(v1.GetSize() == 0);
+
+		assert(v1.Add(cpu));
+		assert(v1.GetSize() == 1);
+
+		assert(!v1.Add(cpp));
+		assert(v1.GetSize() == 1);
+
+		assert(!v1.Remove(cpp));
+		assert(v1.GetSize() == 1);
+
+		assert(v1.Remove(cpu));
+		assert(v1.GetSize() == 0);
+
+		assert(v1.Add(cpu));
+		assert(v1.GetSize() == 1);
+
+		assert(v1.Get(0) == cpu);
+		assert(v1[0] == cpu);
+
+		v1[0] = cpp;
+
+		assert(v1.Get(0) == cpp);
+		assert(v1.GetIndex(cpu) == -1);
+		assert(v1.GetIndex(cpp) == 0);
+	}
+
+	// example test on function details
+	{
+		FixedVector<int, 4> v1;
+
+		assert(v1.Add(1));
+		assert(v1.Add(2));
+		assert(v1.Add(3));
+		assert(v1.Add(4));
+		assert(!v1.Add(5));
+
+		assert(v1.Remove(3));
+		assert(!v1.Remove(3));
+
+		assert(v1.Get(0) == 1);
+		assert(v1[0] == 1);
+
+		assert(v1.GetIndex(2) == 1);
+		assert(v1.GetIndex(5) == -1);
+
+		assert(v1.GetSize() == 3);
+		assert(v1.GetCapacity() == 4);
+	}
+
+	// example test
+	{
+		int i1 = 23;
+		int i2 = 25;
+		int i3 = 10;
+		int i4 = -4;
+		int i5 = 70;
+
+		FixedVector<int, 33> iv;
+
+		assert(iv.GetCapacity() == 33);
+
+		iv.Add(i1);
+		iv.Add(i2);
+		iv.Add(i3);
+		iv.Add(i4);
+
+		assert(iv.GetSize() == 4);
+		assert(iv.GetIndex(i2) == 1);
+		assert(iv.Get(1) == i2);
+		assert(iv[1] == i2);
+
+		bool bRemoved = iv.Remove(i5);
+		assert(!bRemoved);
+		assert(iv.GetSize() == 4);
+
+		bRemoved = iv.Remove(i2);
+		assert(bRemoved);
+		assert(iv.GetSize() == 3);
+		assert(iv.GetIndex(i2) == -1);
+
+		FixedVector<bool, 10> v;
+
+		v.Add(true);
+		v.Add(true);
+		v.Add(true);
+		v.Add(true);
+		v.Add(false);
+		v.Add(false);
+		v.Add(true);
+
+		assert(v.GetSize() == 7);
+		assert(v.Get(0));
+		assert(v.Get(1));
+		assert(v.Get(2));
+		assert(v.Get(3));
+		assert(!v.Get(4));
+		assert(!v.Get(5));
+		assert(v.Get(6));
+
+		v.Remove(false);
+
+		assert(v.GetSize() == 6);
+		assert(!v.Get(4));
+		assert(v.Get(5));
+
+		v.Remove(false);
+
+		assert(v.GetSize() == 5);
+
+		for (size_t i = 0; i < v.GetSize(); ++i)
+		{
+			assert(v[i]);
+		}
+
+		v.Remove(true);
+
+		assert(v.GetSize() == 4);
+
+		for (size_t i = 0; i < v.GetSize(); ++i)
+		{
+			assert(v[i]);
+		}
+
+		FixedVector<bool, 65> v1;
+
+		for (int i = 0; i < 65; ++i)
+		{
+			v1.Add(false);
+		}
+
+		assert(!v1.Add(true));
+		assert(v1.GetSize() == 65);
+		assert(v1.GetIndex(true) == -1);
+	}
+
+	// vector containing many bool data tests
+	{
+		enum
+		{
+			BOOL_CAPACITY = 64
+		};
+
+		FixedVector<bool, BOOL_CAPACITY> v1;
+
+		for (size_t i = 0; i < BOOL_CAPACITY; ++i)
+		{
+			assert(v1.Add(true));
+			assert(v1.GetSize() == i + 1);
+		}
+
+		for (size_t i = 0; i < BOOL_CAPACITY; ++i)
+		{
+			assert(v1.Remove(true));
+			assert(v1.GetSize() == BOOL_CAPACITY - (i + 1));
+		}
+
+		assert(v1.GetSize() == 0);
+
+		for (size_t i = 0; i < 13; ++i)
+		{
+			assert(v1.Add(true));
+			assert(v1.Add(true));
+			assert(v1.Add(true));
+			assert(v1.Add(false));
+			assert(v1.Add(false));
+		}
+
+		for (size_t i = 0; i < BOOL_CAPACITY; ++i)
+		{
+			v1.Remove(false);
+		}
+	}
+}
 
 void testFixedBoolVector()
 {
@@ -184,140 +430,157 @@ void testFixedVector()
 
 int main()
 {
-	int integerArray1[] = { 23, 25, 10, -4, 70 };
-	enum { INTEGER_VECTOR1_SIZE = 4 };
+	FixedVector<bool, 20> v1;
 
-	FixedVector<int, 4> integerVector1;
+	v1.Add(true);
+	v1.Add(true);
+	v1.Add(false);
+	v1.Add(false);
+	v1.Add(true);
+	v1.Add(true);
+	v1.Add(false);
+	v1.Add(false);
+	v1.Add(true);
 
-	assert(integerVector1.GetCapacity() == INTEGER_VECTOR1_SIZE);
-	assert(integerVector1.GetSize() == 0);
-	std::cout << "Test FixedVector constructor: PASS" << std::endl;
+	int8_t myflags = 0b10000101;
+	int8_t option4 = 0b00010000;
 
-	assert(integerVector1.Add(integerArray1[0]) == true);
-	assert(integerVector1.Add(integerArray1[1]) == true);
-	assert(integerVector1.Add(integerArray1[2]) == true);
-	assert(integerVector1.Add(integerArray1[3]) == true);
-	assert(integerVector1.GetSize() == INTEGER_VECTOR1_SIZE);
-	std::cout << "Test FixedVector Add(): PASS" << std::endl;
-	assert(integerVector1.Add(integerArray1[4]) == false);
-	assert(integerVector1.GetSize() == INTEGER_VECTOR1_SIZE);
-	std::cout << "Test FixedVector Add() - over array range: PASS" << std::endl;
+	if (myflags & option4)
+		std::cout << "myflags has option 4 set";
 
-	integerVector1.Add(integerArray1[0]);
-	integerVector1.Add(integerArray1[1]);
-	integerVector1.Add(integerArray1[2]);
-	integerVector1.Add(integerArray1[3]);
-	assert(integerVector1.GetIndex(integerArray1[1]) == 1);
-	std::cout << "Test FixedVector GetIndex(): PASS" << std::endl;
-	assert(integerVector1.Get(1) == integerArray1[1]);
-	std::cout << "Test FixedVector Get(): PASS" << std::endl;
-	assert(integerVector1[1] == integerArray1[1]);
-	std::cout << "Test FixedVector operator[](): PASS" << std::endl;	integerVector1[0] = integerArray1[1];
-	assert(integerVector1[0] == integerArray1[1]);
-	integerVector1[0] = integerArray1[0];
-	assert(integerVector1[0] == integerArray1[0]);
-	std::cout << "Test FixedVector operator[]() - return reference: PASS" << std::endl;
+	//int integerArray1[] = { 23, 25, 10, -4, 70 };
+	//enum { INTEGER_VECTOR1_SIZE = 4 };
 
-	for (size_t i = 0; i < INTEGER_VECTOR1_SIZE; ++i)
-	{
-		assert(integerVector1.Remove(integerArray1[i]) == true);
-		assert(integerVector1.GetSize() == INTEGER_VECTOR1_SIZE - (i + 1));
-		for (size_t j = 0; j < integerVector1.GetSize(); ++j)
-		{
-			assert(integerVector1[j] == integerArray1[i + j + 1]);
-		}
-	}
-	std::cout << "Test FixedVector Remove() - order not changed after remove data: PASS" << std::endl;
-	assert(integerVector1.Remove(integerArray1[4]) == false);
-	assert(integerVector1.GetSize() == INTEGER_VECTOR1_SIZE - 4);
-	std::cout << "Test FixedVector Remove() - empty array: PASS" << std::endl;
+	//FixedVector<int, 4> integerVector1;
 
-	enum { BOOL_VECTOR1_SIZE = 7 };
-	bool boolArray1[] = {
-		true,	/* 0 */
-		true,	/* 1 */
-		true,	/* 2 */
-		true,	/* 3 */
-		false,	/* 4 */
-		false,	/* 5 */
-		true,	/* 6 */
-		true	/* 7 - exceeded data */
-	};
-	FixedVector<bool, BOOL_VECTOR1_SIZE> boolVector1;
-	assert(boolVector1.GetCapacity() == BOOL_VECTOR1_SIZE);
-	assert(boolVector1.GetSize() == 0);
-	std::cout << "Test FixedBoolVector constructor: PASS" << std::endl;
+	//assert(integerVector1.GetCapacity() == INTEGER_VECTOR1_SIZE);
+	//assert(integerVector1.GetSize() == 0);
+	//std::cout << "Test FixedVector constructor: PASS" << std::endl;
 
-	// 가상 소멸자를 만들면 이 테스트는 통과 못하지만 빌드봇은 통과시킴
-	//assert(sizeof(boolVector1) == (sizeof(int32_t) + sizeof(size_t)));
+	//assert(integerVector1.Add(integerArray1[0]) == true);
+	//assert(integerVector1.Add(integerArray1[1]) == true);
+	//assert(integerVector1.Add(integerArray1[2]) == true);
+	//assert(integerVector1.Add(integerArray1[3]) == true);
+	//assert(integerVector1.GetSize() == INTEGER_VECTOR1_SIZE);
+	//std::cout << "Test FixedVector Add(): PASS" << std::endl;
+	//assert(integerVector1.Add(integerArray1[4]) == false);
+	//assert(integerVector1.GetSize() == INTEGER_VECTOR1_SIZE);
+	//std::cout << "Test FixedVector Add() - over array range: PASS" << std::endl;
 
-	std::cout << "Test FixedBoolVector constructor - fit memroy: PASS" << std::endl;
+	//integerVector1.Add(integerArray1[0]);
+	//integerVector1.Add(integerArray1[1]);
+	//integerVector1.Add(integerArray1[2]);
+	//integerVector1.Add(integerArray1[3]);
+	//assert(integerVector1.GetIndex(integerArray1[1]) == 1);
+	//std::cout << "Test FixedVector GetIndex(): PASS" << std::endl;
+	//assert(integerVector1.Get(1) == integerArray1[1]);
+	//std::cout << "Test FixedVector Get(): PASS" << std::endl;
+	//assert(integerVector1[1] == integerArray1[1]);
+	//std::cout << "Test FixedVector operator[](): PASS" << std::endl;	integerVector1[0] = integerArray1[1];
+	//assert(integerVector1[0] == integerArray1[1]);
+	//integerVector1[0] = integerArray1[0];
+	//assert(integerVector1[0] == integerArray1[0]);
+	//std::cout << "Test FixedVector operator[]() - return reference: PASS" << std::endl;
 
-	assert(boolVector1.Add(boolArray1[0]) == true);
-	assert(boolVector1.Add(boolArray1[1]) == true);
-	assert(boolVector1.Add(boolArray1[2]) == true);
-	assert(boolVector1.Add(boolArray1[3]) == true);
-	assert(boolVector1.Add(boolArray1[4]) == true);
-	assert(boolVector1.Add(boolArray1[5]) == true);
-	assert(boolVector1.Add(boolArray1[6]) == true);
-	assert(boolVector1.GetSize() == 7);
-	assert(boolVector1.Get(0) == boolArray1[0]);
-	assert(boolVector1.Get(1) == boolArray1[1]);
-	assert(boolVector1.Get(2) == boolArray1[2]);
-	assert(boolVector1.Get(3) == boolArray1[3]);
-	assert(boolVector1.Get(4) == boolArray1[4]);
-	assert(boolVector1.Get(5) == boolArray1[5]);
-	assert(boolVector1.Get(6) == boolArray1[6]);
-	std::cout << "Test FixedBoolVector Add(): PASS" << std::endl;
-	assert(boolVector1.Add(boolArray1[7]) == false);
-	assert(boolVector1.GetSize() == 7);
-	std::cout << "Test FixedBoolVector Add() - over array range: PASS" << std::endl;
+	//for (size_t i = 0; i < INTEGER_VECTOR1_SIZE; ++i)
+	//{
+	//	assert(integerVector1.Remove(integerArray1[i]) == true);
+	//	assert(integerVector1.GetSize() == INTEGER_VECTOR1_SIZE - (i + 1));
+	//	for (size_t j = 0; j < integerVector1.GetSize(); ++j)
+	//	{
+	//		assert(integerVector1[j] == integerArray1[i + j + 1]);
+	//	}
+	//}
+	//std::cout << "Test FixedVector Remove() - order not changed after remove data: PASS" << std::endl;
+	//assert(integerVector1.Remove(integerArray1[4]) == false);
+	//assert(integerVector1.GetSize() == INTEGER_VECTOR1_SIZE - 4);
+	//std::cout << "Test FixedVector Remove() - empty array: PASS" << std::endl;
 
-	assert(boolVector1.Remove(false) == true);
-	assert(boolVector1.GetSize() == 6);
-	assert(boolVector1.Get(4) == boolArray1[5]);
-	assert(boolVector1.Get(5) == boolArray1[6]);
-	assert(boolVector1.Remove(false) == true);
-	assert(boolVector1.GetSize() == 5);
-	assert(boolVector1.Get(4) == boolArray1[6]);
-	std::cout << "Test FixedBoolVector Remove(): PASS" << std::endl;
-	assert(boolVector1.Remove(false) == false);
-	assert(boolVector1.GetSize() == 5);
-	std::cout << "Test FixedBoolVector Remove() - not found: PASS" << std::endl;
+	//enum { BOOL_VECTOR1_SIZE = 7 };
+	//bool boolArray1[] = {
+	//	true,	/* 0 */
+	//	true,	/* 1 */
+	//	true,	/* 2 */
+	//	true,	/* 3 */
+	//	false,	/* 4 */
+	//	false,	/* 5 */
+	//	true,	/* 6 */
+	//	true	/* 7 - exceeded data */
+	//};
+	//FixedVector<bool, BOOL_VECTOR1_SIZE> boolVector1;
+	//assert(boolVector1.GetCapacity() == BOOL_VECTOR1_SIZE);
+	//assert(boolVector1.GetSize() == 0);
+	//std::cout << "Test FixedBoolVector constructor: PASS" << std::endl;
 
-	for (size_t i = 0; i < boolVector1.GetSize(); i++)
-	{
-		assert(boolVector1[i]);
-	}
-	boolVector1.Remove(true);
-	assert(boolVector1.GetSize() == 4);
-	for (size_t i = 0; i < boolVector1.GetSize(); i++)
-	{
-		assert(boolVector1[i]);
-	}
-	std::cout << "Test FixedBoolVector operator[]() - read: PASS" << std::endl;
+	//// 가상 소멸자를 만들면 이 테스트는 통과 못하지만 빌드봇은 통과시킴
+	////assert(sizeof(boolVector1) == (sizeof(int32_t) + sizeof(size_t)));
 
-	FixedVector<bool, 65> boolVector2;
+	//std::cout << "Test FixedBoolVector constructor - fit memroy: PASS" << std::endl;
+
+	//assert(boolVector1.Add(boolArray1[0]) == true);
+	//assert(boolVector1.Add(boolArray1[1]) == true);
+	//assert(boolVector1.Add(boolArray1[2]) == true);
+	//assert(boolVector1.Add(boolArray1[3]) == true);
+	//assert(boolVector1.Add(boolArray1[4]) == true);
+	//assert(boolVector1.Add(boolArray1[5]) == true);
+	//assert(boolVector1.Add(boolArray1[6]) == true);
+	//assert(boolVector1.GetSize() == 7);
+	//assert(boolVector1.Get(0) == boolArray1[0]);
+	//assert(boolVector1.Get(1) == boolArray1[1]);
+	//assert(boolVector1.Get(2) == boolArray1[2]);
+	//assert(boolVector1.Get(3) == boolArray1[3]);
+	//assert(boolVector1.Get(4) == boolArray1[4]);
+	//assert(boolVector1.Get(5) == boolArray1[5]);
+	//assert(boolVector1.Get(6) == boolArray1[6]);
+	//std::cout << "Test FixedBoolVector Add(): PASS" << std::endl;
+	//assert(boolVector1.Add(boolArray1[7]) == false);
+	//assert(boolVector1.GetSize() == 7);
+	//std::cout << "Test FixedBoolVector Add() - over array range: PASS" << std::endl;
+
+	//assert(boolVector1.Remove(false) == true);
+	//assert(boolVector1.GetSize() == 6);
+	//assert(boolVector1.Get(4) == boolArray1[5]);
+	//assert(boolVector1.Get(5) == boolArray1[6]);
+	//assert(boolVector1.Remove(false) == true);
+	//assert(boolVector1.GetSize() == 5);
+	//assert(boolVector1.Get(4) == boolArray1[6]);
+	//std::cout << "Test FixedBoolVector Remove(): PASS" << std::endl;
+	//assert(boolVector1.Remove(false) == false);
+	//assert(boolVector1.GetSize() == 5);
+	//std::cout << "Test FixedBoolVector Remove() - not found: PASS" << std::endl;
+
+	//for (size_t i = 0; i < boolVector1.GetSize(); i++)
+	//{
+	//	assert(boolVector1[i]);
+	//}
+	//boolVector1.Remove(true);
+	//assert(boolVector1.GetSize() == 4);
+	//for (size_t i = 0; i < boolVector1.GetSize(); i++)
+	//{
+	//	assert(boolVector1[i]);
+	//}
+	//std::cout << "Test FixedBoolVector operator[]() - read: PASS" << std::endl;
+
+	//FixedVector<bool, 65> boolVector2;
 
 
-	// 가상 소멸자를 만들면 이 테스트는 통과 못하지만 빌드봇은 통과시킴
-	//assert(sizeof(boolVector2) == ((sizeof(int32_t) * 3) + sizeof(size_t)));
+	//// 가상 소멸자를 만들면 이 테스트는 통과 못하지만 빌드봇은 통과시킴
+	////assert(sizeof(boolVector2) == ((sizeof(int32_t) * 3) + sizeof(size_t)));
 
-	std::cout << "Test FixedBoolVector constructor - fit memroy: PASS" << std::endl;
-	for (int i = 0; i < 65; i++)
-	{
-		boolVector2.Add(false);
-	}
+	//std::cout << "Test FixedBoolVector constructor - fit memroy: PASS" << std::endl;
+	//for (int i = 0; i < 65; i++)
+	//{
+	//	boolVector2.Add(false);
+	//}
 
-	assert(boolVector2.Add(true) == false);
-	assert(boolVector2.GetSize() == 65);
-	assert(boolVector2.GetIndex(true) == -1);
-	std::cout << "Test FixedBoolVector GetIndex(): PASS" << std::endl;
+	//assert(boolVector2.Add(true) == false);
+	//assert(boolVector2.GetSize() == 65);
+	//assert(boolVector2.GetIndex(true) == -1);
+	//std::cout << "Test FixedBoolVector GetIndex(): PASS" << std::endl;
 
-	testFixedBoolVector();
-	testFixedVector();
-
-	
+	//testFixedBoolVector();
+	//testFixedVector();
+	//test1();
 
 }
 
