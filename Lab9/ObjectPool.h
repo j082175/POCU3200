d09501d68c62b9b364047585653ccf6b9d13bffc
JUNSOOCHO <j082175@naver.com>
2022-11-14 12:02:09 +0900

    lab9_1727

--- Lab9/ObjectPool.h
+++ Lab9/ObjectPool.h
@@ -1,73 +1,61 @@
 #pragma once
 
-#include "IceCube.h" // 지우기
+#include <queue>
 
-template<typename T> // T는 클래스라고 가정, 복사, 대입 불가능, 필요하면 소멸자 구현
-class ObjectPool
+namespace lab9
 {
-public:
-	ObjectPool(size_t maxPoolSize)
-		: mMaxPoolSize(maxPoolSize)
-		, mCurrentCount(0)
+	template<typename T> // T는 클래스라고 가정, 복사, 대입 불가능, 필요하면 소멸자 구현
+	class ObjectPool
 	{
-		mArr = new T*[mMaxPoolSize];
-	}
-
-	~ObjectPool()
-	{
-		//delete[] mArr;
-	}
-
-	ObjectPool(const ObjectPool& other) = delete;
-	ObjectPool& operator=(const ObjectPool& other) = delete;
-
-	T* Get() const
-	{
-		if (mCurrentCount == 0)
+	public:
+		ObjectPool(size_t maxPoolSize)
+			: mMaxPoolSize(maxPoolSize)
+			, mCurrentCount(0)
 		{
-			return new T;
 		}
 
-		size_t max = mArr[0]->GetLiveCount();
-		size_t index = 0;
-
-		for (size_t i = 1; i < mCurrentCount; i++)
+		~ObjectPool()
 		{
-			if (max < mArr[i]->GetLiveCount())
+			//delete[] mArr;
+		}
+
+		ObjectPool(const ObjectPool& other) = delete;
+		ObjectPool& operator=(const ObjectPool& other) = delete;
+
+		T* Get() const
+		{
+			if (mCurrentCount == 0)
 			{
-				max = mArr[i]->GetLiveCount();
-				index = i;
+				return new T;
 			}
+
+			return mArr.front();
 		}
 
-		return mArr[index];
-	}
-
-	void Return(T* other)
-	{
-		if (mCurrentCount == mMaxPoolSize)
+		void Return(T* other)
 		{
-			delete other;
+			if (mCurrentCount == mMaxPoolSize)
+			{
+				delete other;
+			}
+
+			mArr.push(other);
+			mCurrentCount++;
 		}
 
-		mArr[mCurrentCount++] = other;
-	}
+		size_t GetFreeObjectCount() const
+		{
+			return mCurrentCount;
+		}
 
-	size_t GetFreeObjectCount() const
-	{
-		return mCurrentCount;
-	}
+		size_t GetMaxFreeObjectCount() const
+		{
+			return mMaxPoolSize;
+		}
 
-	size_t GetMaxFreeObjectCount() const
-	{
-		return mMaxPoolSize;
-	}
-
-
-
-private:
-	size_t mMaxPoolSize;
-	size_t mCurrentCount;
-	T** mArr;
-};
-
+	private:
+		size_t mMaxPoolSize;
+		size_t mCurrentCount;
+		std::queue<T*> mArr;
+	};
+}
